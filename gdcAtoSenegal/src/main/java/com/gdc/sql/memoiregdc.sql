7a0946391380f3daee2d405e46e5064a78393e86
ahmetdamecisse <ahmet.dame@hotmail.fr>
2016-09-06 10:05:24 +0000

    Spring sécurity marche maintenant 14 base de donnees modifiee

--- gdcAtoSenegal/src/main/java/com/gdc/sql/memoiregdc.sql
+++ gdcAtoSenegal/src/main/java/com/gdc/sql/memoiregdc.sql
@@ -1,10 +1,9 @@
-
 -- phpMyAdmin SQL Dump
 -- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 19 Août 2016 à 17:35
+-- Généré le: Mar 06 Septembre 2016 à 10:04
 -- Version du serveur: 5.5.24-log
 -- Version de PHP: 5.4.3
 
@@ -76,14 +75,9 @@ CREATE TABLE IF NOT EXISTS `bdd` (
 --
 
 INSERT INTO `bdd` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(32, 'domaine1', NULL),
-(34, 'SQL server', NULL),
-(39, 'Oracle', NULL),
-(39, 'SQL server', NULL),
-(40, 'Oracle', NULL),
-(40, 'SQL server', NULL),
-(43, 'Oracle', NULL),
-(43, 'SQL server', NULL);
+(4, 'PLSQL', '2'),
+(4, 'SQL', '1'),
+(4, 'SQL  Server', '3');
 
 -- --------------------------------------------------------
 
@@ -103,11 +97,9 @@ CREATE TABLE IF NOT EXISTS `candidat` (
 --
 
 INSERT INTO `candidat` (`username`, `idTypeDeProfil`) VALUES
-('doudou6', 32),
-('doudou8', 34),
-('Abdoulaye', 39),
-('username1', 40),
-('username3', 43);
+('baba', 1),
+('baba2', 2),
+('baba3', 4);
 
 -- --------------------------------------------------------
 
@@ -131,7 +123,9 @@ CREATE TABLE IF NOT EXISTS `cherchercandidat` (
 CREATE TABLE IF NOT EXISTS `consulternotification` (
   `username` varchar(150) NOT NULL,
   `idNotification` int(11) NOT NULL,
-  PRIMARY KEY (`username`,`idNotification`)
+  PRIMARY KEY (`username`,`idNotification`),
+  KEY `FK_consulternotification1` (`username`),
+  KEY `FK_consulternotification2` (`idNotification`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -173,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `entretien` (
 CREATE TABLE IF NOT EXISTS `experiencesprofessionnelles` (
   `idTypeDeProfil` int(11) NOT NULL,
   `description` varchar(50000) DEFAULT NULL,
-  `datededebut` date NOT NULL DEFAULT '0000-00-00',
+  `datededebut` date NOT NULL,
   `datefin` date NOT NULL,
   `fonction` varchar(650) DEFAULT NULL,
   `environnement` varchar(3000) DEFAULT NULL,
@@ -187,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `experiencesprofessionnelles` (
 --
 
 INSERT INTO `experiencesprofessionnelles` (`idTypeDeProfil`, `description`, `datededebut`, `datefin`, `fonction`, `environnement`, `projet`, `missionsRealisees`) VALUES
-(43, NULL, '2016-08-12', '2016-08-12', NULL, NULL, NULL, NULL);
+(4, 'Optimisation de logiciel de traitement de son', '2016-08-01', '2016-08-31', 'Implémentation d''algorithme d''optimisation numerique', 'Assembleur, algorithme d''optimisation classique, mathématiques avancées', 'Optimisation en temps de calculs et en mémoire d''un outils de traitement du son.', 'Conception de nouveaux algorithmes.');
 
 -- --------------------------------------------------------
 
@@ -253,14 +247,12 @@ CREATE TABLE IF NOT EXISTS `formation` (
 --
 
 INSERT INTO `formation` (`idTypeDeProfil`, `nomDiplome`, `universite`, `anneeObtention`, `mention`) VALUES
-(34, 'Licence', NULL, NULL, NULL),
-(34, 'Master', NULL, NULL, NULL),
-(39, 'Licence', NULL, NULL, NULL),
-(39, 'Master', NULL, NULL, NULL),
-(40, 'Licence', NULL, NULL, NULL),
-(40, 'Master', NULL, NULL, NULL),
-(43, 'Licence', NULL, NULL, NULL),
-(43, 'Master', NULL, NULL, NULL);
+(2, 'Licence', 'Thiès', '2016-08-30', 'Passable'),
+(2, 'Master1', 'UCAD', '2016-08-25', 'Passable'),
+(2, 'Master2', 'UCAD', '2016-08-10', 'Bien'),
+(4, 'LicenceBaba3', 'Thiès', '2016-08-30', 'Passable'),
+(4, 'Master1Baba3', 'UCAD', '2016-08-25', 'Passable'),
+(4, 'Master2Baba3', 'UCAD', '2016-08-10', 'Bien');
 
 -- --------------------------------------------------------
 
@@ -280,12 +272,9 @@ CREATE TABLE IF NOT EXISTS `langages` (
 --
 
 INSERT INTO `langages` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(39, 'java', NULL),
-(39, 'XML', NULL),
-(40, 'java', NULL),
-(40, 'XML', NULL),
-(43, 'java', NULL),
-(43, 'XML', NULL);
+(4, 'JEE', '2'),
+(4, 'PHP', '3'),
+(4, 'XML', '2');
 
 -- --------------------------------------------------------
 
@@ -305,12 +294,9 @@ CREATE TABLE IF NOT EXISTS `langues` (
 --
 
 INSERT INTO `langues` (`idTypeDeProfil`, `nom`, `niveau`) VALUES
-(39, 'En', NULL),
-(39, 'fr', NULL),
-(40, 'En', NULL),
-(40, 'fr', NULL),
-(43, 'En', NULL),
-(43, 'fr', NULL);
+(4, 'Anglais', 'Lu'),
+(4, 'Espagnol', 'Lu'),
+(4, 'Français', 'Lu');
 
 -- --------------------------------------------------------
 
@@ -354,12 +340,9 @@ CREATE TABLE IF NOT EXISTS `materielssystemesexploitation` (
 --
 
 INSERT INTO `materielssystemesexploitation` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(39, 'Linux', NULL),
-(39, 'Windows', NULL),
-(40, 'Linux', NULL),
-(40, 'Windows', NULL),
-(43, 'Linux', NULL),
-(43, 'Windows', NULL);
+(4, 'Lunux', '3'),
+(4, 'MAC OS', '3'),
+(4, 'Windows', '2');
 
 -- --------------------------------------------------------
 
@@ -379,12 +362,9 @@ CREATE TABLE IF NOT EXISTS `methodologie` (
 --
 
 INSERT INTO `methodologie` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(39, 'metho1', NULL),
-(39, 'metho2', NULL),
-(40, 'metho1', NULL),
-(40, 'metho2', NULL),
-(43, 'metho1', NULL),
-(43, 'metho2', NULL);
+(4, 'CMMI1', '3'),
+(4, 'CMMI2', '3'),
+(4, 'CMMI3', '1');
 
 -- --------------------------------------------------------
 
@@ -404,12 +384,9 @@ CREATE TABLE IF NOT EXISTS `modelisation` (
 --
 
 INSERT INTO `modelisation` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(39, 'Model1', NULL),
-(39, 'Model2', NULL),
-(40, 'Model1', NULL),
-(40, 'Model2', NULL),
-(43, 'Model1', NULL),
-(43, 'Model2', NULL);
+(4, 'MERISE', '3'),
+(4, 'Talbot et Rivard', '2'),
+(4, 'UML', '1');
 
 -- --------------------------------------------------------
 
@@ -423,11 +400,50 @@ CREATE TABLE IF NOT EXISTS `notification` (
   `destinateur` varchar(254) DEFAULT NULL,
   `destinataire` varchar(6000) DEFAULT NULL,
   `corpsMessage` varchar(50000) DEFAULT NULL,
-  `pj` mediumblob NOT NULL,
+  `pj` mediumblob,
   `dateNotification` datetime DEFAULT NULL,
   PRIMARY KEY (`idNotification`),
   KEY `FK_envoyerNotification` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `notification`
+--
+
+INSERT INTO `notification` (`idNotification`, `username`, `destinateur`, `destinataire`, `corpsMessage`, `pj`, `dateNotification`) VALUES
+(2, 'ahmet', NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offresemploi`
+--
+
+CREATE TABLE IF NOT EXISTS `offresemploi` (
+  `idOffreEmploi` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(150) NOT NULL,
+  `intitulePoste` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `description` mediumtext CHARACTER SET utf8,
+  `typeDeContrat` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `categorie` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `profilRecherche` varchar(1000) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `diplomeDemandes` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `LanguesParlees` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  `debutContrat` date DEFAULT NULL,
+  `fincontrat` date DEFAULT NULL,
+  `prerequisDuPoste` varchar(1000) DEFAULT NULL,
+  `descriptifDeLaSociete` varchar(3000) DEFAULT NULL,
+  `nbreDanneesDexperiences` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idOffreEmploi`),
+  KEY `FK_publierOffreEmploi` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `offresemploi`
+--
+
+INSERT INTO `offresemploi` (`idOffreEmploi`, `username`, `intitulePoste`, `description`, `typeDeContrat`, `categorie`, `profilRecherche`, `diplomeDemandes`, `LanguesParlees`, `debutContrat`, `fincontrat`, `prerequisDuPoste`, `descriptifDeLaSociete`, `nbreDanneesDexperiences`) VALUES
+(1, 'ahmet', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -447,12 +463,31 @@ CREATE TABLE IF NOT EXISTS `outils` (
 --
 
 INSERT INTO `outils` (`idTypeDeProfil`, `domaine`, `niveau`) VALUES
-(39, 'outil 1', NULL),
-(39, 'outil 2', NULL),
-(40, 'outil 1', NULL),
-(40, 'outil 2', NULL),
-(43, 'outil 1', NULL),
-(43, 'outil 2', NULL);
+(4, 'maven', '2'),
+(4, 'svn', '3'),
+(4, 'Tomcat', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `postuleroffresemploi`
+--
+
+CREATE TABLE IF NOT EXISTS `postuleroffresemploi` (
+  `username` varchar(150) NOT NULL,
+  `idOffreEmploi` int(11) NOT NULL,
+  PRIMARY KEY (`username`,`idOffreEmploi`),
+  KEY `FK_postulerOffreEmploi1` (`username`),
+  KEY `FK_postulerOffreEmploi12` (`idOffreEmploi`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `postuleroffresemploi`
+--
+
+INSERT INTO `postuleroffresemploi` (`username`, `idOffreEmploi`) VALUES
+('baba2', 1),
+('baba3', 1);
 
 -- --------------------------------------------------------
 
@@ -467,23 +502,16 @@ CREATE TABLE IF NOT EXISTS `profil` (
   `competencesFonctionnelles` varchar(1000) DEFAULT NULL,
   `etatProfil` varchar(254) DEFAULT NULL,
   PRIMARY KEY (`idTypeDeProfil`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `profil`
 --
 
 INSERT INTO `profil` (`idTypeDeProfil`, `nombreAnneesExperience`, `principalesCompetences`, `competencesFonctionnelles`, `etatProfil`) VALUES
-(32, NULL, NULL, NULL, NULL),
-(33, NULL, NULL, NULL, NULL),
-(34, NULL, NULL, NULL, NULL),
-(35, NULL, NULL, NULL, NULL),
-(36, NULL, NULL, NULL, NULL),
-(37, NULL, NULL, NULL, NULL),
-(38, NULL, NULL, NULL, NULL),
-(39, NULL, NULL, NULL, NULL),
-(40, NULL, NULL, NULL, NULL),
-(43, NULL, NULL, NULL, NULL);
+(1, NULL, NULL, NULL, NULL),
+(2, 1, '1', '1', NULL),
+(4, 5, 'Cordination2 de projet, SIG', 'Protection2 sociale, CMMI', NULL);
 
 -- --------------------------------------------------------
 
@@ -502,16 +530,9 @@ CREATE TABLE IF NOT EXISTS `profilgl` (
 --
 
 INSERT INTO `profilgl` (`idTypeDeProfil`, `version`) VALUES
-(32, 0),
-(33, 0),
-(34, 0),
-(35, 0),
-(36, 0),
-(37, 0),
-(38, 0),
-(39, 0),
-(40, 0),
-(43, 0);
+(1, NULL),
+(2, NULL),
+(4, NULL);
 
 -- --------------------------------------------------------
 
@@ -523,6 +544,13 @@ CREATE TABLE IF NOT EXISTS `recruteur` (
   `username` varchar(150) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `recruteur`
+--
+
+INSERT INTO `recruteur` (`username`) VALUES
+('ahmet');
 
 -- --------------------------------------------------------
 
@@ -562,19 +590,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`username`, `nom`, `prenom`, `localisation`, `mail`, `telephone`, `password`, `enabled`, `specialite`, `dateDeNaissance`, `version`) VALUES
-('Abdoulaye', 'DIOP', 'kDiop', 'KM', 'diop@gmail.com', '771047524', 'diop48', 1, 'IGL', '3890-12-20 00:00:00', 1),
 ('ahmet', 'CISSE', 'Ahmet', 'Parcelles', 'ahmet.dame@hotmail.fr', '771047524', 'ahmet', 1, 'g?nie logiciel', '1989-01-09 00:00:00', 1),
-('anonymousUser', 'Utilisateur anonyme', NULL, NULL, NULL, NULL, '', 1, NULL, NULL, NULL),
 ('baba', 'GOUGIABY', 'Baba', 'Pikine', 'baba@gmail.com', '775248596', 'baba', 1, 'RT', '1992-06-07 00:00:00', 1),
+('baba2', 'DIOP', 'Baba', 'KM', 'ahmet.cisse@gmail.com', '(77) 104-75-24', 'baba2', 1, 'Génie logiciel', '2016-08-24 00:00:00', 1),
+('baba3', 'Diop', 'Boubacar', 'Parcelles', 'ahmet.cisse@gmail.com', '(77) 104-78-95', 'baba3', 1, 'Génie logiciel', '2016-08-23 00:00:00', 1),
 ('camara', 'CAMARA', 'Ousmane', 'Kolda', 'ouz@gmail.com', '778529635', 'camara', 1, 'g?nie logiciel', '1990-07-19 00:00:00', 1),
-('doudou4', 'CISSE', 'Doudou', 'KM', 'doudou@gmail.com', '771047524', 'doudou4', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('doudou5', 'CISSE', 'Doudou', 'KM', 'doudou@gmail.com', '771047524', 'doudou5', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('doudou6', 'CISSE', 'kDoudou', 'KM', 'doudou@gmail.com', '771047524', 'doudou5', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('doudou7', 'CISSE', 'kDoudou', 'KM', 'doudou@gmail.com', '771047524', 'doudou7', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('doudou8', 'CISSE', 'kDoudou', 'KM', 'doudou@gmail.com', '771047524', 'doudou8', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('maymouna', 'GUEYE', 'Maymouna', 'UCAD', 'gueye@hotmail.fr', '775025485', 'maymouna', 1, 'g?nie logiciel', '1990-07-25 00:00:00', 1),
-('username1', 'DIOP', 'kDiop', 'KM', 'diop@gmail.com', '771047524', 'diop48', 1, 'IGL', '3890-12-20 00:00:00', 1),
-('username3', 'DIOP', 'kDiop', 'KM', 'diop@gmail.com', '771047524', 'diop48', 1, 'IGL', '3890-12-20 00:00:00', 1);
+('demba', 'DIOP', 'Demba', 'Wakam', 'ahmet.cisse@gmail.com', '(77) 104-75-24', 'demba', 1, 'Réseaux et Télécoms', '2016-08-17 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -589,7 +610,7 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
   PRIMARY KEY (`user_role_id`),
   UNIQUE KEY `uni_username_role` (`role`,`username`),
   KEY `fk_username_idx` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Contenu de la table `user_roles`
@@ -597,12 +618,10 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 
 INSERT INTO `user_roles` (`user_role_id`, `username`, `role`) VALUES
 (1, 'ahmet', 'ROLE_ADMIN'),
-(11, 'ahmet', 'ROLE_ANONYMOUS'),
-(10, 'anonymousUser', 'ROLE_ANONYMOUS'),
-(12, 'baba', 'ROLE_ANONYMOUS'),
-(13, 'camara', 'ROLE_ANONYMOUS'),
 (7, 'ahmet', 'ROLE_CANDIDAT'),
 (4, 'baba', 'ROLE_CANDIDAT'),
+(10, 'baba2', 'ROLE_CANDIDAT'),
+(11, 'baba3', 'ROLE_CANDIDAT'),
 (6, 'ahmet', 'ROLE_RECRUTEUR'),
 (5, 'camara', 'ROLE_RECRUTEUR');
 
@@ -648,6 +667,13 @@ ALTER TABLE `candidat`
 --
 ALTER TABLE `cherchercandidat`
   ADD CONSTRAINT `FK_chercherCandidat` FOREIGN KEY (`username`) REFERENCES `candidat` (`username`);
+
+--
+-- Contraintes pour la table `consulternotification`
+--
+ALTER TABLE `consulternotification`
+  ADD CONSTRAINT `FK_consulternotification1` FOREIGN KEY (`username`) REFERENCES `candidat` (`username`),
+  ADD CONSTRAINT `FK_consulternotification2` FOREIGN KEY (`idNotification`) REFERENCES `notification` (`idNotification`);
 
 --
 -- Contraintes pour la table `entretien`
@@ -716,10 +742,23 @@ ALTER TABLE `notification`
   ADD CONSTRAINT `FK_envoyerNotification` FOREIGN KEY (`username`) REFERENCES `recruteur` (`username`);
 
 --
+-- Contraintes pour la table `offresemploi`
+--
+ALTER TABLE `offresemploi`
+  ADD CONSTRAINT `FK_publierOffreEmploi` FOREIGN KEY (`username`) REFERENCES `recruteur` (`username`);
+
+--
 -- Contraintes pour la table `outils`
 --
 ALTER TABLE `outils`
   ADD CONSTRAINT `FK_association11` FOREIGN KEY (`idTypeDeProfil`) REFERENCES `profilgl` (`idTypeDeProfil`);
+
+--
+-- Contraintes pour la table `postuleroffresemploi`
+--
+ALTER TABLE `postuleroffresemploi`
+  ADD CONSTRAINT `FK_postulerOffreEmploi1` FOREIGN KEY (`username`) REFERENCES `candidat` (`username`),
+  ADD CONSTRAINT `FK_postulerOffreEmploi2` FOREIGN KEY (`idOffreEmploi`) REFERENCES `offresemploi` (`idOffreEmploi`);
 
 --
 -- Contraintes pour la table `profilgl`
