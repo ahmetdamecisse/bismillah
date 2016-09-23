dac2beee1f204464df234e73fe22646f1db39263
ahmetdamecisse <ahmet.dame@hotmail.fr>
2016-09-23 12:51:13 +0000

    suivi des entrtiens 36

--- gdcAtoSenegal/src/main/java/com/gdc/managed/bean/SuiviEntrtienBean.java
+++ gdcAtoSenegal/src/main/java/com/gdc/managed/bean/SuiviEntrtienBean.java
@@ -20,7 +20,7 @@ import org.primefaces.model.ScheduleModel;
 
 @ManagedBean
 @ViewScoped
-public class ScheduleView implements Serializable {
+public class SuiviEntrtienBean implements Serializable {
 
     private ScheduleModel eventModel;
 
@@ -29,10 +29,10 @@ public class ScheduleView implements Serializable {
     @PostConstruct
     public void init() {
         eventModel = new DefaultScheduleModel();
-        eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", previousDay8Pm(), previousDay11Pm()));
-        eventModel.addEvent(new DefaultScheduleEvent("Birthday Party", today1Pm(), today6Pm()));
-        eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));
-        eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
+        eventModel.addEvent(new DefaultScheduleEvent("Entretien Technique avec BabaCar Touré", previousDay8Pm(), previousDay11Pm()));
+        eventModel.addEvent(new DefaultScheduleEvent("Entetien DRH avec Moustapha CISSE", today1Pm(), today6Pm()));
+        eventModel.addEvent(new DefaultScheduleEvent("Entretien Technique avec Maymouna Guéye", nextDay9Am(), nextDay11Am()));
+        eventModel.addEvent(new DefaultScheduleEvent("Entretien DRH avec Mamadou DIOP", theDayAfter3Pm(), fourDaysLater3pm()));
 
     }
 
